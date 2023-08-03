9b9d1ed204f89b13f1742d9f74c7a1b1e45304cc
timefractal <me@timefractal.com>
2023-08-04 00:01:25 +0530

    Add data about hours for each jira ticket

--- app.py
+++ app.py
@@ -26,11 +26,32 @@ def index_page():
     for project in projects:
         progress_report_data[project.key] = {}
         to_do_issues = jira.search_issues("project="+project.key+" and status='To Do'")
+        
+        to_do_issues_hours = 0
+        for to_do_issue in to_do_issues:
+            if to_do_issue.fields.customfield_10016 is not None:
+                to_do_issues_hours = to_do_issues_hours + float(to_do_issue.fields.customfield_10016)
+        
         progress_report_data[project.key]['to_do'] = len(to_do_issues)
+        progress_report_data[project.key]['to_do_hours'] = to_do_issues_hours
+        
         in_progress_issues = jira.search_issues("project="+project.key+" and status='In Progress'")
+        in_progress_issues_hours = 0
+        for in_progress_issue in in_progress_issues:
+            if in_progress_issue.fields.customfield_10016 is not None:
+                in_progress_issues_hours = in_progress_issues_hours + float(in_progress_issue.fields.customfield_10016)
+        
         progress_report_data[project.key]['in_progress'] = len(in_progress_issues)
+        progress_report_data[project.key]['in_progress_hours'] = in_progress_issues_hours
+
         total_issues = jira.search_issues("project="+project.key)
+        total_issues_hours = 0
+        for total_issue in total_issues:
+            if total_issue.fields.customfield_10016 is not None:
+                total_issues_hours = total_issues_hours + float(total_issue.fields.customfield_10016)
+        
         progress_report_data[project.key]['total'] = len(total_issues)
+        progress_report_data[project.key]['total_hours'] = total_issues_hours
 
     jira_progress_report.document("1").update(progress_report_data)
     return render_template('base.html', name="JIRA Progress Report")
